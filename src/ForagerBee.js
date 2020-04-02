969a4edefc3a44c77fd904363be7c6e605d81449
Charlie Thao <cthao309@gmail.com>
2020-04-01 18:15:33 -0700

    Comppleted 6ees6ees6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor() {
+    super();
+
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(value = '$pollen$') {
+    this.treasureChest.push(value);
+  }
 };
